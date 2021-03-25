9dc07606fe5534b87c31f683424b67677a86875c
Schimmoellerm <schimmoellerm@gmail.com>
2021-03-25 18:18:30 -0400

    tied in inquirer and require, added in first 6 questions to the prompts

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
 
 // TODO: Create an array of questions for user input
+inquirer
+.prompt([
+    {type: 'input', message: "What is the title of your project?", name: 'inputTitle'},
+
+    {type: 'input', message: "Write a description of your project.", name: 'description'},
+
+    {type: 'input', message: "Provide installation instructions for your project.", name: 'installation_instructions'},
+
+    {type: 'input', message: "Provide usage information for your project.", name: "usage_information"},
+
+    {type: 'input', message: "Provide contribution information regarding your project.", name: "contribution_information"},
+
+    {type: 'input', message: "Provide test instructions for your project", name: "test_instructions"},
+
+    
+
+
+])
 const questions = [];
 
 // TODO: Create a function to write README file
